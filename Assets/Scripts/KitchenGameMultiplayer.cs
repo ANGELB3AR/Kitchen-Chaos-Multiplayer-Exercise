6822bbfdf903002a2ecaebe1537709943c3e0717
Angel Bowers <ANGELLBOWERS@GMAIL.COM>
2023-03-22 13:36:33 -0500

    Handled late joining

--- Assets/Scripts/KitchenGameMultiplayer.cs
+++ Assets/Scripts/KitchenGameMultiplayer.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using Unity.Netcode;
@@ -13,6 +14,27 @@ public class KitchenGameMultiplayer : NetworkBehaviour
     {
         Instance = this;
     }
+
+
+    public void StartHost()
+    {
+        NetworkManager.Singleton.ConnectionApprovalCallback += NetworkManager_ConnectionApprovalCallback;
+        NetworkManager.Singleton.StartHost();
+    }
+
+    private void NetworkManager_ConnectionApprovalCallback(NetworkManager.ConnectionApprovalRequest connectionApprovalRequest, NetworkManager.ConnectionApprovalResponse connectionApprovalResponse)
+    {
+        bool isApproved = KitchenGameManager.Instance.IsWaitingToStart();
+
+        connectionApprovalResponse.Approved = isApproved;
+        connectionApprovalResponse.CreatePlayerObject = isApproved;
+    }
+
+    public void StartClient()
+    {
+        NetworkManager.Singleton.StartClient();
+    }
+
 
     public void SpawnKitchenObject(KitchenObjectSO kitchenObjectSO, IKitchenObjectParent kitchenObjectParent)
     {
